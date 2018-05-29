d40f8bbde821399a2f38cdef2e6b8137422e590d
PauloG <pmg119@hotmail.com>
2018-05-28 22:21:54 -0700

    working react

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MT Ideas app
+
+### To start developing, clone this repository into your local machine
+### To start developing in the front end, after you have the repository:
+### npm install
+### npm start
---
+++ ideasproj/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Welcome to MT Ideas! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
