1451c15d598d88fe266de57e4e7dbd1dd33a8311
PauloG <pmg119@hotmail.com>
2018-06-07 10:47:25 -0700

    django tutorial pt 2 for db + react feed component

--- ideasproj/ideas/models.py
+++ ideasproj/ideas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Idea(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    idea_id = models.IntegerField(default=0000)
+    def __str__(self):
+        return self.title
---
+++ ideasproj/src/feed/Feed.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Feed.css";
+
+class Feed extends Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
